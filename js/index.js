6ed948b79740723a62450d359f307dd5b7a0d5f9
Katala-Jonni <katala.jonni@yandex.ru>
2018-03-28 02:06:22 +0300

    Версия №11 Декоративные элементы

--- js/index.js
+++ js/index.js
@@ -1,22 +1,38 @@
 'use strict';
-const controls = document.querySelectorAll('.slider-controls i');
-const sliders = document.querySelectorAll('.slider-list li');
-controls.forEach((btn, i) => {
-  btn.addEventListener('click', event => {
-    controls.forEach(btn => btn.classList.remove('active'));
-    sliders.forEach(slide => slide.style.display = 'none');
-    event.target.classList.toggle('active');
-    sliders[i].style.display = 'block';
-  })
-});
 
-const writeUsButton = document.querySelector('.write-us .button');
-writeUsButton.addEventListener('click', event => {
-  event.preventDefault();
-  document.querySelector('.modal').classList.toggle('hidden');
-});
+window.addEventListener('DOMContentLoaded', showDOMContentLoaded);
 
-const modalClose = document.querySelector('.modal-close');
-modalClose.addEventListener('click', event => {
-  event.target.parentElement.classList.toggle('hidden');
-});
+function showDOMContentLoaded() {
+  const controls = document.querySelectorAll('.slider-controls i');
+  const sliders = document.querySelectorAll('.slider-list li');
+
+  function getFirstSlide() {
+    if (!sliders[0]) {
+      return;
+    } else {
+      sliders[0].classList.remove('visually-hidden');
+    }
+  }
+
+  getFirstSlide();
+
+  controls.forEach((btn, i) => {
+    btn.addEventListener('click', event => {
+      controls.forEach(btn => btn.classList.remove('active'));
+      sliders.forEach(slide => slide.classList.add('visually-hidden'));
+      event.target.classList.toggle('active');
+      sliders[i].classList.remove('visually-hidden');
+    })
+  });
+
+  const writeUsButton = document.querySelector('.write-us .button');
+  writeUsButton.addEventListener('click', event => {
+    event.preventDefault();
+    document.querySelector('.modal').classList.remove('visually-hidden');
+  });
+
+  const modalClose = document.querySelector('.modal-close');
+  modalClose.addEventListener('click', event => {
+    event.target.parentElement.classList.add('visually-hidden');
+  });
+}
